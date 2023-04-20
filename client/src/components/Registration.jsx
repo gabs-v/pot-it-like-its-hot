f5dcc7a416d9f2a677f4d318a34cdd044765a87d
gabs-v <gabyvazquez8297@gmail.com>
2023-04-19 21:38:05 -0700

    client start

---
+++ client/src/components/Registration.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Registration = () => {
+    return (
+        <div id='regPage'>
+            <h1>Pot It Like It's Hot</h1>
+            <h2></h2>
+        </div>
+    )
+}
+
+export default Registration
